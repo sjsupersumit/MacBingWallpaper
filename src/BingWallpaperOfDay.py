278f659184690e256de92c5460c2d784f91b4031
sjsupersumit <sjsupersumit@gmail.com>
2016-06-19 12:36:46 +0530

    Downloads from flickr rather than bing based on tags

--- src/BingWallpaperOfDay.py
+++ src/BingWallpaperOfDay.py
@@ -6,16 +6,20 @@ import json
 import datetime
 import subprocess
 import os
+import ConfigParser
 from appscript import *
 
 
-BING_URL = "http://www.bing.com"
-BASE_DIR = "/Users/sumit.jha/Desktop/wallpaper"
-
+BING_URL= "http://www.bing.com"
+BASE_DIR= "/Users/sumit.jha/Desktop/wallpaper"
 
 def main():
-    file_path = download_image()
-    set_wallpaper(file_path)
+    try:
+        file_path = download_from_flickr()
+        set_wallpaper(file_path)
+    except Exception as e:
+        print "Exception occured..." + str(e)
+
 
 
 def download_image():
@@ -45,7 +49,50 @@ def download_image():
             return file_path
 
 
+def download_from_flickr():
+    flickr_api_base_url = "https://api.flickr.com/services/rest/"
+    config = ConfigParser.ConfigParser()
+    dir = os.path.dirname(__file__)
+    config.read(os.path.join(dir, 'settings.conf'))
+    key = config.get('Flickr', 'API_KEY')
+    params = {
+        'api_key': key,
+        'tags': ['bliss','outdoors', 'beautiful', 'wallpaper', 'hd', 'motivation', 'inspiration', 'exotic'],
+        'tag_mode': 'any',
+        'safe_search': '2',
+        'content_type': '1',
+        'format':'json',
+        'method': 'flickr.photos.search',
+        'nojsoncallback': '1'
+    }
+    response = requests.get(flickr_api_base_url, params)
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        total =  int(data['photos']['total'])
+        if total > 100:
+            total = 100
+        image_no = randint(0,total)
+        photo_detail = data['photos']['photo'][image_no]
+        photo_id = str(photo_detail['id'])
+        owner = photo_detail['owner']
+        farm_id = str(photo_detail['farm'])
+        server_id = str(photo_detail['server'])
+        secret = str(photo_detail['secret'])
+        flickr_photo_url = "https://farm" + farm_id + ".staticflickr.com/" + server_id + "/" + photo_id +"_" + secret + "_h.jpg"
+        response = requests.get(flickr_photo_url)
+        if response.status_code == 200:
+            if not os.path.exists(BASE_DIR) :
+                os.makedirs(BASE_DIR)
+            file_path = BASE_DIR + "/" + photo_id + ".jpg"
+            f = open(file_path, 'wb')
+            f.write(response.content)
+            f.close()
+            return file_path
+
 def set_wallpaper(file_path):
+
+    if file_path is None:
+        return
     SCRIPT = """/usr/bin/osascript<<END
                 ignoring application responses
                 tell application "Finder"
